564a0f9c549a49dbc1a3086c979d3a5de223c09c
Shuang LI <Shuang.LI@amadeus.com>
2023-01-31 11:54:52 +0800

    Objected oriented basics - exercises

--- src/main/scala/lectures/part2oop/OOBasics.scala
+++ src/main/scala/lectures/part2oop/OOBasics.scala
@@ -8,6 +8,20 @@ object OOBasics extends App {
   println(person.x)
   person.greet("Daniel")
   person.greet()
+
+  // test exercises
+  val author = new Writer("Charles", "Dickens", 1812)
+  val imposter = new Writer("Charles", "Dickens", 1812)
+  val novel = new Novel("Great Expecations", 1861, author)
+
+  println(novel.authorAge) // syntax allowed for param-less methods
+  println(novel.isWrittenBy(author))
+  println(novel.isWrittenBy(imposter))
+
+  val counter = new CounterWithSideEffect
+  counter.increment.print
+  counter.increment.increment.increment.print
+  counter.increment(10).print
 }
 
 // constructor
@@ -30,4 +44,81 @@ class Person(name: String, val age: Int) {
   // the next constructor must call primary or secondary constructor. Hence overloading constructors
   // are quite limited in usage (only good for adding default params. But one could use default
   // param in the class signature.
+}
+
+/*
+Novel & Writer class
+Writer: first name, surname, year
+  - method: full name
+Novel: name, year of release, author
+  - authorAge (at the year of release)
+  - isWrittenBy(author): if the novel is written by author
+  - copy (new year of release) = new instance of Novel with a new year of release
+ */
+
+/* my solution
+class Writer(firstName: String, surname: String, year: Int) {
+  val birthYear: Int = year
+
+  def fullName(): String = {
+    this.firstName + this.surname
+  }
+}
+ */
+
+// ans
+class Writer(firstName: String, surname: String, val year: Int) {
+  def fullName = firstName + " "+ surname // notice that there is no () for params. No "this" either
+}
+
+
+class Novel(name: String, yearOfRelease: Int, val author: Writer) {
+  def authorAge: Int = yearOfRelease - author.year
+
+  def isWrittenBy(author: Writer): Boolean = author == this.author
+
+  def copy(newYearOfRelease: Int): Novel = new Novel(name, newYearOfRelease, author)
+}
+
+/*
+Counter class
+  - receive an int value
+  - method: current count
+  - method: increment / decrement -> new Counter
+  - overload inc/dec to receive an amount -> new Counter
+ */
+
+class Counter(val count: Int) { // with val, a getter isn't needed any more
+  def increment: Counter = new Counter(count + 1) // immutability. instances are fixed, cannot be modified inside
+  // whenever content needs modification, a new instance is returned. V important concept
+
+  def decrement: Counter = new Counter(count - 1)
+
+  def increment(amount: Int): Counter = new Counter(count + amount)
+
+  def decrement(amount: Int): Counter = new Counter(count - amount)
+}
+
+class CounterWithSideEffect(val count: Int = 0) { // with val, a getter isn't needed any more
+  def increment: CounterWithSideEffect = {
+    println("Incrementing")
+    new CounterWithSideEffect(count + 1)
+  }
+
+  def decrement: CounterWithSideEffect = {
+    println("Decrementing")
+    new CounterWithSideEffect(count - 1)
+  }
+
+  def increment(amount: Int): CounterWithSideEffect = {
+    if (amount <= 0) this
+    else increment.increment(amount - 1) // increment method returns a new instance -> this instance calls increment(amount - 1)
+  }
+
+  def decrement(amount: Int): CounterWithSideEffect = {
+    if (amount <= 0) this
+    else decrement.decrement(amount - 1)
+  }
+
+  def print = println(count)
 }
